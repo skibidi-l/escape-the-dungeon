a9cc1f8d8b8c185c6e1250b97a698721fa8c01b1
skibidi-l <htet_myat_aung_1@students.edu.sg>
2025-09-04 14:04:56 +0000

    lesson 15 methods

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import random
 
+class Equipment:
+    def __init__(self, weapon, armor):
+        self.weapon = weapon
+        self.armor = armor
+
 class Attributes:
     def __init__(self, strength, agility, mind):
         self.strength = strength
@@ -7,12 +12,27 @@ class Attributes:
         self.mind = mind
 
 class Character:
-    def __init__(self, name, attributes):
+    def __init__(self, name, character_class , attributes):
         self.name = name
+        self.character_class = character_class
         self.max_health = attributes.strength * 5
         self.current_health = self.max_health
         self.attributes = attributes    
         self.equipments = None
+    
+    def equip(self, equipment):
+        self.equipments = equipment
+    
+    def get_armor_value(self):
+        if self.equipments != None:
+            return armor_value[self.equipments.armor]
+        return 0
+    
+    def attack(self, target):
+        damage = self.attributes.strength + random.randint(1, 6)
+        damage = damage - target.get_armor_value()
+        target.current_health -= target.current_health - damage
+        return damage
 
 class PlayerCharacter(Character):
     def __init__(self, name, character_class, attributes):
@@ -24,12 +44,14 @@ class NonPlayerCharacter(Character):
     def __init__(self, name, character_class, attributes):
         super().__init__(name, character_class, attributes)
 
-npc = Character("smily",30,30, Attributes(5, 3, 2))
+npc = Character("smily", "npc", Attributes(5, 3, 2))
 print(f"NPC Name: {npc.name}")
 print(f"NPC Max Health: {npc.max_health}")
 print(f"NPC Current Health: {npc.current_health}")
 
-hero = PlayerCharacter("hero","warrior", 40, Attributes(6,4,3))
+hero = PlayerCharacter("hero","warrior", Attributes(6,4,3))
+hero.equip(Equipment("sword","leather armor"))
+
 
 def roll_dice( sides_per_die, number_of_dice=1):
     total = 0
@@ -77,12 +99,12 @@ elif class_choice == "2":
     print("yes...a sneaky assasin that punishes foes for stepping on his shadow!")
 elif class_choice == "3":
     player_class = "Mage"
-    attr = Attributes(8,4,2,)
+    attr = Attributes(8,4,2)
     print("yes...a mystical wizard who bends the very fabric of reality to his will!")
 else:
     print("you made a wrong move lul, idc if u made it by mistake but since you thought smashing your head on the keyboard was a option your getting the WOMP WOMP class, try not to die!!!")
     player_class = "WOMP WOMP"
-    strength, agility, mind = 1, 1, 1
+    attr = Attributes(3,3,3)
 player = {
     "name":character_name,
     "class": player_class,
@@ -95,8 +117,8 @@ player = {
         "armor": "leather armor",  # Default armor
     },
     # Health is calculated based on strength
-    "max_health": 5 * strength,
-    "current_health": 5 * strength,
+    "max_health": 5 * attr.strength,
+    "current_health": 5 * attr.strength,
     "gold": 5,
     "inventory": []
 }
@@ -120,7 +142,7 @@ skeleton_monster = {
     "current_health": 20,
 }
 
-_skeleton_monster = NonPlayerCharacter("skeleton", Attributes(4, 2, 0))
+_skeleton_monster = NonPlayerCharacter("skeleton","undead", Attributes(4, 2, 0))
 
 goblin_monster = {
     "name": "goblin",
@@ -190,7 +212,7 @@ while True:
         print(f"the Monster's health is {monster['current_health']}")
 
     elif action == "dodge":
-        dodge_chance = agility * 5
+        dodge_chance = attr.agility * 5
         if roll_dice(sides_per_die=100) <= dodge_chance:
             is_dodged = True
             print("ya dodged the attack, well played...")
@@ -200,7 +222,7 @@ while True:
             player["current_health"] -= 5
             print(f"ya got hit, HP remaining:{player["current_health"]}")
     elif action == "spell":
-        if mind>=6:
+        if attr.mind>=6:
             print("ya cast a powerful fireball")
             monster["current_health"] <= 0
             print("well done ")
