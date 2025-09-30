af6802dcf68fbc9d83744d5129a8a484f0282c47
skibidi-l <htet_myat_aung_1@students.edu.sg>
2025-09-30 14:07:28 +0000

    lesson 19 rooms

--- game.py
+++ game.py
@@ -104,6 +104,8 @@ class Character:
     def start_combat(self):
         self.is_in_combat = True
 
+    def end_combat(self):
+        self.is_in_combat = False
 class PlayerCharacter(Character):
     def __init__(self, name, character_class, attributes):
         super().__init__(name, character_class, attributes)
@@ -127,3 +129,59 @@ def loot_roll(loot_list):
         loot_item = random.choice(loot_list)
         looted_items.append(loot_item)
     return looted_items
+
+def encounter(player, monster):
+    print(f"You have encountered a {monster.name}!")
+    player.start_combat()
+    
+    print(f"welcome to the dungeon dungeoneer, to test your might, you will have to duel...a good ole {monster.name}!")
+    print(f"You have encountered a {monster.name}!")   
+    print(f"⚠️ The {monster.name} attacks you!⚠️")
+
+    loot_list = ["cloth armor","leather armor","chainmail armor","plate armor","dragon scale","sword","dagger","staff","mace","axe"]
+    while True:
+        if player.current_health <= 0:
+            break
+        action = input("Choose action: attack / dodge / spell: ").lower()
+        has_dodged = False
+        
+        if action == 'attack':
+            damage = player.attack(monster)
+            print(f"You swing your weapon and deal {damage} damage!")
+
+            print(f"the Monster's health is {monster.current_health}")
+
+        elif action == "dodge":
+            has_dodged = player.dodge()
+            if has_dodged:
+                print("ya dodged the attack, well played...")
+            else:
+                print("ya tried to dodge but had a skill issue,lul")
+        elif action == "spell":
+            if player.attributes.mind>=6:
+                print("ya cast a powerful fireball")
+                monster.current_health = 0
+                print("well done ")
+            else:
+                print("ya fail to cast the spell.")
+        else:
+            print("dumdass dont smash your keyboard!!!")
+        if monster.current_health <= 0:
+            print(f"congrats,your SOOOO good, well this is only thy beginning, you have slain a {monster.name}!")
+            print("you may earn these items:")
+            for loot in loot_list:
+                print(loot)
+            print("LET'S GO GAMBLINGGG!!! picking your rewards...")
+            number_of_loot_items = roll_dice
+            looted_items = loot_roll(loot_list)
+            loot_gold = roll_dice(number_of_dice=5, sides_per_die=4)
+            print(f"you found {looted_items} and {loot_gold} gold from the {monster.name}, u RICH now")
+            player.inventory.extend(looted_items)
+            player.gold += loot_gold
+            break
+        else:
+            if not has_dodged:
+                hit = monster.attack(player)
+                print(f"The {monster.name} brandishes a knife, hitting a mighty swing dealing {hit} your player health depleted to {player.current_health}")
+
+    player.end_combat()
--- main.py
+++ main.py
@@ -102,60 +102,33 @@ rooms = {
         'west': "Armory",
     }
 }
-monster = random.choice(monster_list)
-print(f"welcome to the dungeon dungeoneer, to test your might, you will have to duel...a good ole {monster.name}!")
-print(f"You have encountered a {monster.name}!")   
-print(f"⚠️ The {monster.name} attacks you!⚠️")
+current_room = 'cell'
 
-
-loot_list = ["cloth armor","leather armor","chainmail armor","plate armor","dragon scale","sword","dagger","staff","mace","axe"]
+print("You find yourself in a dungeon cell. Your goal is to escape and grasp freedom!")
+print("type 'exit' to exit the game at any time.")
 while True:
-    if player.current_health <= 0:
-        break
-    action = input("Choose action: attack / dodge / spell: ").lower()
-    has_dodged = False
+    print(f"\nYou are in the {current_room}.")
+    print(rooms[current_room]['description'])
     
-    if action == 'attack':
-        damage = player.attack(monster)
-        print(f"You swing your weapon and deal {damage} damage!")
-
-        print(f"the Monster's health is {monster.current_health}")
-
-    elif action == "dodge":
-        has_dodged = player.dodge()
-        if has_dodged:
-            print("ya dodged the attack, well played...")
-        else:
-            print("ya tried to dodge but had a skill issue,lul")
-    elif action == "spell":
-        if attr.mind>=6:
-            print("ya cast a powerful fireball")
-            monster.current_health = 0
-            print("well done ")
-        else:
-            print("ya fail to cast the spell.")
-    else:
-        print("dumdass dont smash your keyboard!!!")
-    if monster.current_health <= 0:
-        print(f"congrats,your SOOOO good, well this is only thy beginning, you have slain a {monster.name}!")
-        print("you may earn these items:")
-        for loot in loot_list:
-            print(loot)
-        print("LET'S GO GAMBLINGGG!!! picking your rewards...")
-        number_of_loot_items = game.roll_dice
-        looted_items = game.loot_roll(loot_list)
-        loot_gold = game.roll_dice(number_of_dice=5, sides_per_die=4)
-        print(f"you found {looted_items} and {loot_gold} gold from the {monster.name}, u RICH now")
-        player.inventory.extend(looted_items)
-        player.gold += loot_gold
+    if 'item' in rooms[current_room]:
+        print(f"You see a {rooms[current_room]['item']} here.")
+    
+    action = input("Where do you wish to go?(go [direction] / take [item] / stats / exit): ").strip().lower()
+    
+    if action == 'exit':
+        print("Exiting the game. Goodbye!")
         break
-    else:
-        if not has_dodged:
-            hit = monster.attack(player)
-            print(f"The {monster.name} brandishes a knife, hitting a mighty swing dealing {hit} your player health depleted to {player.current_health}")
-if player.current_health <= 0:
-    print("GAME OVER!")
-    print("LOL WOMP WOMP")
-else:
-    print("hey man, this game is made as a draft for me coding class so tysm for playin, hope to see ya again!!!😊")
-    print(f" you now have {player.inventory} in your inventory and {player.gold} gold")
+    elif action == 'stats':
+        player.show_stats()
+        continue
+
+# monster = random.choice(monster_list)
+# game.encounter(player, monster)
+
+
+# if player.current_health <= 0:
+#     print("GAME OVER!")
+#     print("LOL WOMP WOMP")
+# else:
+#     print("hey man, this game is made as a draft for me coding class so tysm for playin, hope to see ya again!!!😊")
+#     print(f" you now have {player.inventory} in your inventory and {player.gold} gold")
